6916e63216d169f809d4a4f98f46470389974f59
Jahnavi <gaurjahnavi84@gmail.com>
2025-12-26 14:23:39 +0530

    change 1

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -11,7 +11,7 @@ const TodoList = ({ todos, handleComplete }) => {
 
             {!todo.isComplete && (
               <button onClick={() => handleComplete(todo.id)}>
-                Complete {todo.id}
+                Complete
               </button>
             )}
           </li>
